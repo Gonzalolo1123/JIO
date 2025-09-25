5c799f0cc39c533d1c300f23a011c765c2c3351c
Sergio Martínez <104039933+sergiodmartinezf@users.noreply.github.com>
2025-09-25 15:28:25 -0300

    configuracion para postgres

--- JIO/settings.py
+++ JIO/settings.py
@@ -73,11 +73,26 @@ WSGI_APPLICATION = 'JIO.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# SQLITE
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+# POSTGRESQL
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+       'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'jiodb',
+       'USER': 'postgres',
+       'PASSWORD': 'compuvi',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
 
 
