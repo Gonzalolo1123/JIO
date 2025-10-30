c2fcdfb98357e5ca87183cb919159e9632c886d1
Damian C <damian.15carrillod@gmail.com>
2025-10-30 02:12:19 -0300

    Fix: ajustar dependencia de 0007 a 0002 para resolver NodeNotFoundError

--- jio_app/migrations/0007_alter_juego_estado.py
+++ jio_app/migrations/0007_alter_juego_estado.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('jio_app', '0006_alter_juego_categoria_alter_juego_foto'),
+        ('jio_app', '0002_alter_juego_precio_base'),
     ]
 
     operations = [
